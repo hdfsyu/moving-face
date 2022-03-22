b2f2cb9c5f39ee57fc452841bb2cb53e26147479
Anton Ego <viyaanchauhan@hotmail.com>
2022-03-22 15:02:28 -0400

    made a background

--- README.md
+++
@@ -1,9 +0,0 @@
-# Moving Face
-This is my first gameboy game, I am working on more features (obviously I'm not gonna be keeping the project like this)
-# Compiling
-First, you must have extracted the GBDK zip in C:\
-Then run the following command in Command Prompt: `make.bat` Or in pwsh (powershell) `.\make.bat`
-You should see a file named "main.gb" if you see it, success! You compiled the game.
-# Running
-If you have an emulator, simply load the ROM file into the emulator and it should work properly
-If you have an actual gameboy, use a flash cartridge to load the game onto the gameboy.
--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include <gb/gb.h>
 #include <stdio.h>
 #include "FaceSprites.c"
+#include "background.c"
+#include "backgroundmap.c"
 void main(){
 	UINT8 currentspriteindex = 0;
+	set_bkg_data(0, 7, backgroundtiles);
+	set_bkg_tiles(0, 0, 40, 18, backgroundmap);
 	set_sprite_data(0, 2, Faces);
 	set_sprite_tile(0, 0);
 	move_sprite(0, 88, 78);
+	SHOW_BKG;
 	SHOW_SPRITES;
+	DISPLAY_ON;
 	while(1){
 		switch(joypad()){
 			case J_LEFT:
@@ -22,6 +28,7 @@ void main(){
 				scroll_sprite(0, 0, 5);
 				break;
 		}
+		scroll_bkg(1,0);
 		delay(100); // please dont remove this otherwise you get an uncontrollable sonic tile
 	}
 }
